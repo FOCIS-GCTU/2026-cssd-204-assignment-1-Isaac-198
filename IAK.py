3f890942f710ed2855f83891912b27a43e783498
Isaac-198 <2425300419@live.gctu.edu.gh>
2026-05-30 12:58:59 +0000

    Add files via upload
    Scripting languages Assignment 2

---
+++ IAK.py
@@ -0,0 +1,32 @@
+# File: IAK.py
+# Description: Print out my initials in large stylized letters
+# Assignment 1
+
+    # Name: Isaac Asante konadu
+    # Index No: 2425300419
+    # Email: 2425300419@live.gctu.edu.gh
+    # Grader: Augustus Buckman
+
+    #On my honor, Isaac Asante Konadu, this Programming assignment is my own work
+    #and I have not provided this code to any other student
+
+def main():
+            # This program prints my initials in stylized large letters
+
+            print()
+            print(" ...IAk")
+            print()
+
+            print(" ...IIIIIIIIIIII...........AAAAA............KKK......KKK.....")
+            print(" .......IIII.............AAAA.AAAA..........KKK.....KKK......")
+            print(" .......IIII............AAA....AAAA.........KKK....KKK.......")
+            print(" .......IIII............AAA....AAAA.........KKK...KKK........")
+            print(" .......IIII............AAAAAAAAAAA.........KKK..KKK.........")
+            print(" .......IIII............AAA.....AAA.........KKK.KKK..........")
+            print(" .......IIII............AAA.....AAA.........KKK..KKK.........")
+            print(" .......IIII............AAA.....AAA.........KKK...KKK........")
+            print(" .......IIII.......**...AAA.....AAA....**...KKK....KKK.....**")
+            print(" ...IIIIIIIIIIII...**...AAA.....AAA....**...KKK......KKk...**")
+
+main()
+
